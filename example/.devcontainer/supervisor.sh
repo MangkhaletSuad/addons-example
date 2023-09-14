e5190560426219a80fbced718b6fa765a23b2e12
RVCONNEX\mangkhalet.s <mangkhalet.s@rvconnex.com>
2023-09-14 10:30:07 +0700

    Test_17

--- example/.devcontainer/supervisor.sh
+++ example/.devcontainer/supervisor.sh
@@ -1,16 +1,20 @@
 docker run \
-  --rm \
-  -it \
-  --name builder \
-  --privileged \
-  -v /path/to/addon:/data \
-  -v /var/run/docker.sock:/var/run/docker.sock:ro \
-  homeassistant/amd64-builder \
-  -t /data \
-  --all \
-  --test \
-  -i my-test-addon-{arch} \
-  -d local
+  --rm --privileged \
+        --name hassio_supervisor \
+        --privileged \
+        --security-opt seccomp=unconfined \
+        --security-opt apparmor:unconfined \
+        -v /run/docker.sock:/run/docker.sock:rw \
+        -v /run/dbus:/run/dbus:ro \
+        -v /run/udev:/run/udev:ro \
+        -v /tmp/supervisor_data:/data:rw \
+        -v "$WORKSPACE_DIRECTORY":/data/addons/local:rw \
+        -v /etc/machine-id:/etc/machine-id:ro \
+        -e SUPERVISOR_SHARE="/tmp/supervisor_data" \
+        -e SUPERVISOR_NAME=hassio_supervisor \
+        -e SUPERVISOR_DEV=1 \
+        -e SUPERVISOR_MACHINE="qemux86-64" \
+        "homeassistant/amd64-hassio-supervisor:${SUPERVISOR_VERSON}"
 
 docker build \
   --build-arg BUILD_FROM="homeassistant/amd64-base:latest" \
