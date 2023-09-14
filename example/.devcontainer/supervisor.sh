121bfde8e4cabf3f9d69bf985d539eaa3850e4e2
RVCONNEX\mangkhalet.s <mangkhalet.s@rvconnex.com>
2023-09-14 11:11:27 +0700

    test_18

--- example/.devcontainer/supervisor.sh
+++ example/.devcontainer/supervisor.sh
@@ -1,28 +1,5 @@
-docker run \
-  --rm --privileged \
-        --name hassio_supervisor \
-        --privileged \
-        --security-opt seccomp=unconfined \
-        --security-opt apparmor:unconfined \
-        -v /run/docker.sock:/run/docker.sock:rw \
-        -v /run/dbus:/run/dbus:ro \
-        -v /run/udev:/run/udev:ro \
-        -v /tmp/supervisor_data:/data:rw \
-        -v "$WORKSPACE_DIRECTORY":/data/addons/local:rw \
-        -v /etc/machine-id:/etc/machine-id:ro \
-        -e SUPERVISOR_SHARE="/tmp/supervisor_data" \
-        -e SUPERVISOR_NAME=hassio_supervisor \
-        -e SUPERVISOR_DEV=1 \
-        -e SUPERVISOR_MACHINE="qemux86-64" \
-        "homeassistant/amd64-hassio-supervisor:${SUPERVISOR_VERSON}"
+#!/usr/bin/with-contenv bashio
 
-docker build \
-  --build-arg BUILD_FROM="homeassistant/amd64-base:latest" \
-  -t local/my-test-addon \
-  .
+echo "Hello world!"
 
-docker run \
-  --rm \
-  -v /tmp/my_test_data:/data \
-  -p PORT_STUFF_IF_NEEDED \
-  local/my-test-addon
+python3 -m http.server 4449
